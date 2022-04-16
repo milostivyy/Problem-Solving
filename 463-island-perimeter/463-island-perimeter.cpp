231d95d578ddc860a21f4737a418d94f0e8387ff
Mahima Chauhan <78307937+milostivyy@users.noreply.github.com>
2022-04-16 23:33:07 +0530

    Time: 138 ms (64.92%), Space: 96.2 MB (57.29%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -9,12 +9,11 @@ public:
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
-                {//int cnt=0;
+                {
                 if(i==0 || grid[i-1][j]==0) cnt++;
                 if(j==0 || grid[i][j-1]==0) cnt++;
                 if(i==m-1 || grid[i+1][j]==0) cnt++;
                 if(j==n-1 || grid[i][j+1]==0) cnt++;
-                    //ans+=cnt;
                 }
                     
                 
