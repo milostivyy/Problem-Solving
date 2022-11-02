2cf4a30225feaeff71cc9484e1333f11041afcea
Mahima Chauhan <mahima19197@gmail.com>
2022-11-02 10:31:27 +0530

    Time: 56 ms (49.09%), Space: 13.3 MB (18.98%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    vector<int> findBall(vector<vector<int>>& vec) {
-        vector<int> res ;
-        for(int i =0;i<vec[0].size();i++){
-
-            int  x=0,y=i;
-            bool flag = true;
-            while(x<vec.size()){
-
-                if(vec[x][y]==1){   // if block is directing ball to right
-                 if (y+1>= vec[0].size() || vec[x][y+1]!=1){ // ball will go down only if immediate right directs right otherwise V shape formed
-                   flag = false;
-                   break;       }
-                 x++,y++;
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int>res;
+        for(int i=0;i<grid[0].size();i++){
+            int x=0;
+            int y=i;
+            bool flag=true;
+            while(x<grid.size())
+            {if(grid[x][y]==1){
+                if( y+1>=grid[0].size() || grid[x][y+1]!=1){
+                    flag=false;
+                    break;
+                    
                 }
-
-                else if(vec[x][y]==-1){  // if block is directing to left
-                      if (y-1<0 || vec[x][y-1]!=-1){
-                   flag = false;
-                   break;       }
-                 x++,y--;
-                }
-
+                x++;
+                y++;
             }
-            if(flag)res.push_back(y);
-            else
-                res.push_back(-1);
-
+            else if(grid[x][y]==-1){
+                if( y-1<0||grid[x][y-1]!=-1 ){
+                    flag=false;
+                    break;}
+                x++;
+                y--;
+            }
+            }
+             if(flag) res.push_back(y);
+             else res.push_back(-1);
+            }
+        return res;
         }
-           return res;
-
-        
-    }
 };
